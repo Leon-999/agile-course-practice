429a6d193446714c81e41205c975db3a97ea4d79
Leon-999 <lionewr@gmail.com>
2015-12-04 03:55:00 +0300

    Added class ViewModel and class with tests fot it

---
+++ code/gatchenko-viktor/ViewModel/src/main/java/ru/unn/agile/LongArithmetic/viewmodel/ViewModel.java
@@ -0,0 +1,29 @@
+package ru.unn.agile.LongArithmetic.viewmodel;
+
+import ru.unn.agile.LongArithmetic.model.Matrix;
+
+public class ViewModel {
+
+    private String mFirstMatrix;
+    private String nFirstMatrix;
+    private String mSecondMatrix;
+    private String nSecondMatrix;
+
+    private Matrix firstMultiplier;
+    private Matrix secondMultiplier;
+    private Matrix resultMatrix;
+
+    private String status;
+
+    public ViewModel() {
+
+    }
+
+    private void initializeMatrixes() {
+
+    }
+
+    private void multiplyMatrixes() {
+
+    }
+}
---
+++ code/gatchenko-viktor/ViewModel/src/test/java/ru/unn/agile/LongArithmetic/viewmodel/ViewModelTests.java
@@ -0,0 +1,22 @@
+package ru.unn.agile.LongArithmetic.viewmodel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+}
