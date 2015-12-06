53453c7b2b0e4c48da4f96632c82ae8ba402b762
Leon-999 <lionewr@gmail.com>
2015-12-06 23:24:16 +0300

    Add new keyboard hey - 'M'

--- code/gatchenko-viktor/ViewModel/src/main/java/ru/unn/agile/LongArithmetic/viewmodel/KeyboardKeys.java
+++ code/gatchenko-viktor/ViewModel/src/main/java/ru/unn/agile/LongArithmetic/viewmodel/KeyboardKeys.java
@@ -2,6 +2,7 @@ package ru.unn.agile.LongArithmetic.viewmodel;
 
 public final class KeyboardKeys {
     public static final int ENTER = 10;
+    public static final int M = 77;
     public static final int ANY = 3333;
 
     private KeyboardKeys() { }
